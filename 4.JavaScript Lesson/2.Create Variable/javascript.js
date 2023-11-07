c8b869a37ac976afd849c010850d7d91ddc00217
doeunzx <144660703+doeunzx@users.noreply.github.com>
2023-11-07 13:08:56 +0700

    New commit

---
+++ 4.JavaScript Lesson/2.Create Variable/javascript.js
@@ -0,0 +1,8 @@
+// let nameFull = "I am Sidoeun.";
+// console. log(nameFull + " " + "Vet Sidoeun.\n weer"); 
+// console. log(nameFull + " " + "I am here to study
+// console. log(nameFull);
+ let id,lastName,firstName;
+ console.log(firstName + " " + "Vet");
+ console.log(lastName + " " + "Sidoeun");
+ console.log(id + " " + "12345");
