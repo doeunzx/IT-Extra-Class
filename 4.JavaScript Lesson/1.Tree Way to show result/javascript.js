b206a03b63b363a96ee5813732199b9335bc645c
doeunzx <144660703+doeunzx@users.noreply.github.com>
2023-11-07 10:50:47 +0700

    New commit

---
+++ 4.JavaScript Lesson/1.Tree Way to show result/javascript.js
@@ -0,0 +1,8 @@
+// alert ("Trov tea khom rean");
+document.write("Welcome my page guy!");
+
+console. log("Welcome my page guy!");
+/*
+console. log({name : "Trov tea khom rean"});
+document.getElementById("java").innerHTML = "Hello JavaScript";
+*/
