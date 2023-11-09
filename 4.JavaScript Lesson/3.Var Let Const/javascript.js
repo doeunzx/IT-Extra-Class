c90a0ff4a64f7424400c2df020b48fe0cef6e92b
doeunzx <144660703+doeunzx@users.noreply.github.com>
2023-11-09 18:17:11 +0700

    New commit

---
+++ 4.JavaScript Lesson/3.Var Let Const/javascript.js
@@ -0,0 +1,11 @@
+// var let const
+// "var, let" we can reasign the value but "const" can not be use;
+
+const name = "Doeunzx";
+// name = "Sidoeun";
+var id = 123; 
+let firstName = "Sidoeun";
+    firstName = "Vet Sidoeun";
+console. log( name);
+console. log (id);
+console. log (firstName);
