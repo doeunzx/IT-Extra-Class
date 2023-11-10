10950c14d4e2900b102cb9aa33f74454c7ef645c
doeunzx <144660703+doeunzx@users.noreply.github.com>
2023-11-10 19:32:58 +0700

    new commit

---
+++ 4.JavaScript Lesson/5.Real world problem with number as string/javascript.js
@@ -0,0 +1,5 @@
+let num1 = 13;
+let num2 = "10";
+// console. log(num1 + num2);
+// console. log(num1 - num2);
+console. log(num1 * num2);
---
+++ 4.JavaScript Lesson/6.Operators/javascript.js
@@ -0,0 +1,21 @@
+// let num1 = 20;
+// let num2 = 10;
+// const result = num1 - num2;
+
+let num1 = 10;
+// num1 = 10 + 20;
+num1 += 10 ;
+// num1 /= 10 ;
+// num1 -= 10 ;
+// num1 *= 10 ;
+console. log(num1);
+
+let num2 = 10;
+// num2 += 1;
+// num2 += 1;
+// num2 += 1;
+num2++;
+num2++;
+num2++;
+num2++;
+console. log (num2);
