dc3577be816b7eb2a6386ee3c65cf32ba88b6df3
doeunzx <144660703+doeunzx@users.noreply.github.com>
2023-11-14 00:01:55 +0700

    New  commit

---
+++ 4.JavaScript/7.Datatype/javascript.js
@@ -0,0 +1,23 @@
+let num = 10;
+let str = "This is string datatype";
+let a  = true;
+let myName ;
+console. log(typeof myName);
+console. log ( typeof a);
+console. log  (typeof str);
+console. log (typeof num);
+
+let myName1 = "Vet Sidoeun";
+myName1 = "I am a student";
+console. log (typeof myName1);
+
+let nothing = null; 
+console. log ( typeof nothing );
+const obj = { my_obj : 12};
+console. log ( typeof obj );
+const arr = [ "Sidoeun ", 12 , {welcome:"Reanmore"}];
+console.log ( typeof arr );
+function func() {
+    return null;
+}
+console. log( typeof func);
